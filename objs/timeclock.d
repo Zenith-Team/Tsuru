275d9b63e046fab060b5d7f880b02cbffab04b89
Anarchy <letterable9191@gmail.com>
2021-03-09 15:36:22 -0500

    Cleanup and asm
    - Renamed yamls to be consistent
    - Added timeclock sprite (missing symbols)
    - Added first asm hack, no music speedup after 100 secs

--- objs/timeclock.d
+++ objs/timeclock.d
@@ -1,5 +1,6 @@
-objs\effectspawner.o: source/effectspawner.cpp ../files/include/actor/base.h \
- ../files/include/util/vec2i.h ../files/include/types.h \
+objs\timeclock.o: source/timeclock.cpp ../files/include/actor/actor.h \
+ ../files/include/util/vec3u.h ../files/include/types.h \
+ ../files/include/actor/base.h ../files/include/util/vec2i.h \
  ../files/include/util/vec3.h ../files/include/util/vec2.h \
  ../files/include/math.h ../files/include/sead.h \
  G:\Zenith\Hacks\ghs\multi5327\scxx\cstddef \
@@ -27,12 +28,15 @@ objs\effectspawner.o: source/effectspawner.cpp ../files/include/actor/base.h \
  G:\Zenith\Hacks\ghs\multi5327\scxx\xstddef \
  G:\Zenith\Hacks\ghs\multi5327\scxx\yvals.h ../files/include/sead/resource.h \
  ../files/include/profile.h ../files/include/preprocessor.h \
- ../files/include/profileid.h ../files/include/actor/actor.h \
- ../files/include/util/vec3u.h ../files/include/activecollider.h \
+ ../files/include/profileid.h ../files/include/activecollider.h \
  ../files/include/util/rect.h ../files/include/eatdata.h \
- ../files/include/eventmgr.h ../files/include/effect.h \
- ../files/include/util/mtx34.h ../files/include/movementhandler.h \
- ../files/include/states.h ../files/include/sound.h ../files/include/nw/snd.h
+ ../files/include/movementhandler.h ../files/include/states.h \
+ ../files/include/model.h ../files/include/util/mtx34.h \
+ ../files/include/resarchive.h ../files/include/nw/g3d.h \
+ ../files/include/nw/g3d/res_common.h ../files/include/nw/g3d/res_file.h \
+ ../files/include/drawmgr.h ../files/include/effect.h \
+ ../files/include/sound.h ../files/include/nw/snd.h \
+ ../files/include/eventmgr.h ../files/include/level.h
 
-:cmdList=cxppc  -c  -MD -I../files/include -bsp generic -cpu=espresso -object_dir=objs -Ospeed --g++ --no_debug --no_rtti -Omemfuncs -Ostrfuncs -DCemu -DREGION_NSMBU_US_Latest_Cemu -DCODE_ADDR=0x2b01360 -DDATA_ADDR=0x10607d00 -filetype.cc source/effectspawner.cpp -o objs\effectspawner.o ; 
-:cmdHash=0x48ec52b6
+:cmdList=cxppc  -c  -MD -I../files/include -bsp generic -cpu=espresso -object_dir=objs -Ospeed --g++ --no_debug --no_rtti -Omemfuncs -Ostrfuncs -DCemu -DREGION_NSMBU_US_Latest_Cemu -DCODE_ADDR=0x2b01360 -DDATA_ADDR=0x10607d00 -filetype.cc source/timeclock.cpp -o objs\timeclock.o ; 
+:cmdHash=0x30990326
