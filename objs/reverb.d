4741ae12d6b55a09eebce932f8576e1c251ce719
Anarchy <letterable9191@gmail.com>
2021-03-10 10:56:53 -0500

    Yoshi colors asm patch
    Unlocks pink, lightblue and yellow yoshi. Needs manual nybble 12 modification  for now.

--- objs/reverb.d
+++
@@ -1,5 +0,0 @@
-objs\reverb.o: source/reverb.cpp ../files/include/sound.h \
- ../files/include/nw/snd.h ../files/include/types.h
-
-:cmdList=cxppc  -c  -MD -I../files/include -bsp generic -cpu=espresso -object_dir=objs -Ospeed --g++ --no_debug --no_rtti -Omemfuncs -Ostrfuncs -DCemu -DREGION_NSMBU_US_Latest_Cemu -DCODE_ADDR=0x2b01360 -DDATA_ADDR=0x10607d00 -filetype.cc source/reverb.cpp -o objs\reverb.o ; 
-:cmdHash=0x483087dc
