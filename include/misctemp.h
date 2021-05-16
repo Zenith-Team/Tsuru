0b805ffaeae7004a56868e9989adc9812fcb17f1
Luminyx <79218624+Luminyx1@users.noreply.github.com>
2021-05-16 13:20:24 -0400

    Move ActorScaler class to actormgr.h from misctemp.h

--- include/misctemp.h
+++ include/misctemp.h
@@ -12,17 +12,6 @@ public:
 	u32 _28;		// 28
 };
 
-class ActorScaler {  // size: 0x1C
-public:
-    virtual void getScale();
-
-	Vec3 scale;		// 0
-	f32 _C;			// C
-	u32 _10;		// 10
-	u32 _14;		// 14
-	Actor* owner;	// 18
-};
-
 void mapPosToScreen(Vec2* , Vec2* );
 bool moveValueTo(u32 *, u32 target, u32 amount);	
 bool moveFloatTo(float *, float target, float amount);
