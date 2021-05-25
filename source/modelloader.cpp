b160b8caefc48d333a0e686e0310b7f87a8e0a21
Luminyx <79218624+Luminyx1@users.noreply.github.com>
2021-05-25 12:39:51 -0400

    Add basic Model Loader implementation

--- source/modelloader.cpp
+++ source/modelloader.cpp
@@ -1,8 +1,8 @@
-#include "actor/actorbase.h"
+#include "actor/actor.h"
 
 #include "drawmgr.h"
 
-class ModelLoader : public ActorBase {
+class ModelLoader : public Actor {
 public:
     ModelLoader(const ActorBuildInfo* buildInfo);
     virtual ~ModelLoader() { }
@@ -21,15 +21,41 @@ public:
 const Profile ModelLoaderProfile(&ModelLoader::build, ProfileId::Sprite308, "ModelLoader", nullptr, 0);
 PROFILE_RESOURCES(ProfileId::Sprite308, "modelpack");
 
-ModelLoader::ModelLoader(const ActorBuildInfo* buildInfo)
-    : ActorBase(buildInfo) { }
+ModelLoader::ModelLoader(const ActorBuildInfo* buildInfo) : Actor(buildInfo) { }
 
 ActorBase* ModelLoader::build(const ActorBuildInfo* buildInfo) {
     return new ModelLoader(buildInfo);
 }
 
 u32 ModelLoader::onCreate() {
-    switch (/* Nybble dropdown */) {
-        
+    u8 nybble12 = settings1 >>  0 & 0xF;
+    switch (nybble12) {
+        case 0: model = ModelWrapper::create("modelpack", "glare");
+        default: model = ModelWrapper::create("modelpack", "default");
     }
+
+    updateModel();
+
+    return 1;
+}
+
+u32 ModelLoader::onExecute() {
+    updateModel();
+
+    return 1;
+}
+
+u32 ModelLoader::onDraw() {
+    DrawMgr::instance->drawModel(model);
+
+    return 1;
+}
+
+void ModelLoader::updateModel() {
+    Mtx34 mtx;
+    mtx.rotateAndTranslate(rotation, position);
+
+    model->setMtx(mtx);
+    model->updateModel();
+    model->updateAnimations();
 }
