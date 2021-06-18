4212ed4a063140108b3c7586c564991743d16300
Luminyx1 <letterable9191@gmail.com>
2021-06-17 23:09:15 -0400

    Add stationary pokey patch, remove waves
    Will try to rewrite waves from scratch because it just won't stop crashing

--- source/misc.cpp
+++ source/misc.cpp
@@ -4,3 +4,13 @@ void Bouncy__collisionYoshi(Enemy* _this, HitboxCollider* hcSelf, HitboxCollider
     if (_this->processCollision(hcSelf, hcOther, 1) == 0)
         _this->Enemy::collisionYoshi(hcSelf, hcOther);
 }
+
+void StationaryPokey(Enemy* _this) {
+    // If nybble 8 is checked
+    if ((_this->settings1 >> 16) & 0x1) {
+        _this->speed.setX(0.0f);
+    }
+
+    // restore replaced instruction
+    asm("li r0, -1");
+}
