1bf9f24f1222c7256bf30c9fc7353fac63248422
Luminyx1 <letterable9191@gmail.com>
2021-06-21 23:22:03 -0400

    Comment out unnecessary code and add some labels

--- source/waves.cpp
+++ source/waves.cpp
@@ -3,40 +3,42 @@
 #include "actor/liquid.h"
 
 void Waves_onCreate(EnvTerrain* _this) {
-    LOG("Waves_onCreate");
 
     // Initialize TerrainEffects but we skip because it's already initialized in Water::onCreate()
-    //_this->effects.init(&_this->position, 57600.0f, true, false, TerrainEffects::Water, _this->_11A40);
+  ////_this->effects.init(&_this->position, 57600.0f, true, false, TerrainEffects::Water, _this->_11A40);
 
-    // Set wave values from spritedata
-    _this->effects.setWaveValues(_this->settings2 >> 4 & 0xF, _this->settings2 >> 0x10 & 0xF, _this->settings2 >> 8 & 0xF, _this->settings2 >> 0x14 & 0xF, _this->settings2 >> 0xC & 0xF, _this->settings2 >> 0x18 & 0xF); 
+    // Set wave values from spritedata but we skip it because it does nothing for some reason
+  ////_this->effects.setWaveValues(_this->settings2 >> 4 & 0xF, _this->settings2 >> 0x10 & 0xF, _this->settings2 >> 8 & 0xF, _this->settings2 >> 0x14 & 0xF, _this->settings2 >> 0xC & 0xF, _this->settings2 >> 0x18 & 0xF); 
 
-    // Set top
-    _this->setTop(_this->_1AAC - _this->_1AC8);
+    // Set top but we skip it because it's not required apparently (or is already set)
+  ////_this->setTop(_this->_1AAC - _this->_1AC8);
 
-    _this->effects._E4 = TerrainEffects::Water;   // Set type
+    // Set type but we skip it because it's already set correctly
+  ////_this->effects._E4 = TerrainEffects::Water;
 
-    // TileMgr setup
-    TileMgr::instance->hasWaves = true;
-    TileMgr::instance->waveType = 2;
-
-    LOG("waveSpeed: %x\n", _this->effects._C4);
-    LOG("waveAnimSpeed: %x\n", _this->effects._C8);
-    LOG("waveHeight: %x\n", _this->effects._DC);
-    LOG("waveAnimHeight: %x\n", _this->effects._E0);
-    LOG("type: %x\n", _this->effects._E4);
+    // TileMgr setup but we skip it because it's not required apparently (or is already set)
+  ////TileMgr::instance->hasWaves = true;
+  ////TileMgr::instance->waveType = 2;
 }
 
-void Waves_onExecute(TerrainEffects* _this) {
-    LOG("Waves_onExecute");
+void Waves_onExecute(EnvTerrain* _this) {
 
-    // Update wave collisions
-    _this->updateWaveCollisions();
+}
 
-    LOG("waveHeight: %x\n", _this->_DC);
-    // Set some values
-    //_this->_C4 *= 3;            // 
-    //_this->_C8 = 10;
-    _this->_DC = 10;    // waveHeight
-    //_this->_E0 *= 3;            // 
+void Waves_onExecute(TerrainEffects* _this) {   // TerrainEffects* part of onExecute because calling the subclass doesn't work for some reason
+    u32 wavewidth = _this->_D4;
+
+    // Update wave collisions but we skip it because it already gets called in EnvTerrain::onExecute()
+  ////_this->updateWaveCollisions();
+
+    // Check mario wave hit but we skip it because it treats the waves as lava so we do a:
+    // TODO: Patch this function to treat water waves as water type
+    //_this->checkMarioWaveHit();
+
+    // Set some values for testing
+    //? Values have to be set in onExecute for some reason... probably related to this being a TerrainEffects* instead of an EnvTerrain*
+    _this->_C4 = 0x0;   // waveSpeed
+    _this->_C8 = 0xF;   // waveAnimSpeed
+    _this->_DC = 0xF;   // waveHeight
+    _this->_E0 = 0x30;  // peaks height
 }
