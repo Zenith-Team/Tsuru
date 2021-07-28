4a985e282437d9579435057480fb7441f959d33b
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-07-28 00:48:13 -0300

    broke door shit

--- include/game/actor/doorbase.h
+++ include/game/actor/doorbase.h
@@ -14,7 +14,8 @@ public:
     u32 onExecute() override;
     u32 onDraw() override;
 
-    virtual void vf18C();
+    void vf18C() override; // nullsub (return type on Enemy may be wrong)
+
     virtual void vf4E4(); // deleted
     virtual void ZOrder();
     DECLARE_STATE_VIRTUAL(DoorBase, Unk1);
@@ -22,20 +23,20 @@ public:
     DECLARE_STATE_VIRTUAL(DoorBase, Unk3);
     DECLARE_STATE_VIRTUAL(DoorBase, Unk4);
     virtual bool isCurrentStateUnk3();
-    virtual u32  vf55C();
+    virtual u32  vf55C(); // nullsub
     virtual void vf564(); // deleted
     virtual void vf56C(); // deleted
     virtual void loadModel();
     virtual void initHitboxCollider();
-    virtual void vf584();
-    virtual void vf58C();
+    virtual void vf584(); // nullsub
+    virtual void vf58C(); // nullsub
     virtual void playOpenDoorAnim();
     virtual void playOpenDoorSound();
     virtual void playCloseDoorAnim();
     virtual void playCloseDoorSound();
-    virtual void vf5B4();
-    virtual void vf5BC();
-    virtual u32  vf5C4();
+    virtual void vf5B4(); // nullsub
+    virtual void vf5BC(); 
+    virtual u32  vf5C4(); // nullsub
 
     static void collisionCallback(HitboxCollider* hcSelf, HitboxCollider* hcOther);
 
