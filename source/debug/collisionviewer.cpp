ced197f782d51614457501e65f30887220d07dd9
STUPID Modder <44854972+stupidestmodder@users.noreply.github.com>
2021-07-28 17:47:53 -0300

    fix not printing profile id in debug collision viewer

--- source/debug/collisionviewer.cpp
+++ source/debug/collisionviewer.cpp
@@ -160,9 +160,7 @@ void AreaTask::debugDraw(const agl::lyr::RenderInfo& renderInfo) {
         }
 
         else {
-            #if DEBUG >= 2
             LOG("Found unknown collider for actor with id: 0x%x, and profile id: 0x%x\n", colliderBase->mOwner->mID, colliderBase->mOwner->mProfile->mID);
-            #endif
         }
 
         node = node->mNext;
