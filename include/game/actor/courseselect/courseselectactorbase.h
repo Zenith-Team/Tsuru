c37b42d479adbda622123910058c40ea2d23a174
Luminyx <79218624+Luminyx1@users.noreply.github.com>
2021-08-01 16:03:18 -0400

    Add init comments to courseselectactorbase.h

--- include/game/actor/courseselect/courseselectactorbase.h
+++ include/game/actor/courseselect/courseselectactorbase.h
@@ -12,23 +12,23 @@ public:
     u32 beforeExecute() override;
     u32 beforeDraw() override;
 
-    Vec3f mPosition;
-    Vec3f mPosition2;
-    Vec3f _68;
-    Vec3f _74;
-    Vec3f mScale;
+    Vec3f mPosition;    // Inited to buildInfo->mPosition
+    Vec3f mPosition2;   // Inited to buildInfo->mPosition
+    Vec3f _68;          // Inited to 0.0, 0.0, 0.0
+    Vec3f _74;          // Inited to 0.0, 0.0, 0.0
+    Vec3f mScale;       // Inited to 1.0, 1.0, 1.0
     Vec3f _8C;
     Vec3f _98;
     Vec3f _A4;
-    u32 _B0;
-    u32 mRotation;
-    u32 _B8;
-    u32 _BC;
-    u32 mRotation2;
-    u32 _C4;
-    f32 _C8;
-    f32 _CC;
-    f32 _D0;
-    f32 _D4;
-    bool _D8;
+    u32 _B0;            // Inited to 0
+    u32 mRotation;      // Inited to buildInfo->mRotation
+    u32 _B8;            // Inited to 0
+    u32 _BC;            // Inited to 0
+    u32 mRotation2;     // Inited to buildInfo->mRotation
+    u32 _C4;            // Inited to 0
+    f32 _C8;            // Inited to 0.0
+    f32 _CC;            // Inited to -7.0
+    f32 _D0;            // Inited to -40.0
+    f32 _D4;            // Inited to 0.0
+    bool _D8;           // Inited to false
 };
