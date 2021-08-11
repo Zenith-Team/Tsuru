0fbe3e41e015bcbf1d78265496b76a537732a36f
Luminyx1 <letterable9191@gmail.com>
2021-08-10 22:47:37 -0400

    Finish CourseSelectTask struct

--- include/agl/layer.h
+++ include/agl/layer.h
@@ -15,4 +15,19 @@ public:
     u8 _10[0x41C - 0x10];
 };
 
+class DrawMethod : public sead::IDisposer {
+
+public:
+    sead::SafeString mName;
+    u32 _18;
+    u32 _1C;
+    u32 _20;
+    u32 _24;
+    u8 mMethod[8];  //! This is a PTMF, please make it into one
+    u32 _30;
+    u32 _34;
+    u32 _38;
+    u32 _3C;
+};
+
 } }
