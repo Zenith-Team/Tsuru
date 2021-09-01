e6069305a780fcedd7612b5e7e1917043c0f1e60
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-09-01 20:39:36 -0300

    remove naming prefixes m and p from agl, cafe, custom and dynlibs

--- include/agl/lyr/renderer.h
+++ include/agl/lyr/renderer.h
@@ -10,8 +10,8 @@ class Renderer { // Size: 0xF78
     SEAD_SINGLETON_DISPOSER(Renderer)
 
 public:
-    u8 _10[0xDA8-0x10];             // _10
-    sead::Buffer<Layer*> mLayers;   // _DA8
+    u8 _10[0xD98];                 // _10
+    sead::Buffer<Layer*> layers;   // _DA8
     //...
 };
 
