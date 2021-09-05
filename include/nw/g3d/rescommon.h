3234ca9bf3a072b0c52c520ec4413994c573b383
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-09-05 12:25:11 -0300

    mPrefix removal of nw/g3d

--- include/nw/g3d/rescommon.h
+++ include/nw/g3d/rescommon.h
@@ -5,11 +5,11 @@
 namespace nw { namespace g3d { namespace res {
 
 struct BinaryFileHeader {
-    u8 mSignature[4];   // _0
-    u8 mVersion[4];     // _4
-    u16 mByteOrder;     // _8
-    u16 mHeaderSize;    // _A
-    u32 mFileSize;      // _C
+    u8  signature[4];  // _0
+    u8  version[4];    // _4
+    u16 byteOrder;     // _8
+    u16 headerSize;    // _A
+    u32 fileSize;      // _C
 };
 
 } } }
