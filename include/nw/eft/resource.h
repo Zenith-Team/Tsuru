262e3b7ad555cd9d79deb68776ba1041d70fd5db
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-09-07 14:49:52 -0300

    finish mPrefix removal from nw/eft

--- include/nw/eft/resource.h
+++ include/nw/eft/resource.h
@@ -18,20 +18,20 @@ struct VertexShaderKey;
 class Resource { // Size: 0x30
 public:
     struct EmitterSet { // Size: 0x38
-        EmitterSetData* mData;
-        EmitterTblData* mEmitterRef;
-        u32 mNumEmitter;
-        u32 mUserData;
-        EmitterSetData* mData2;
-        EmitterTblData* mEmitterRef2;
-        u32 mNumEmitter2;
-        u32 mUserData2;
-        const char* mName;
-        u8 mUnused;
-        ParticleShader** mShaders;
-        u32 mNumShader;
-        Primitive** mPrimitives;
-        u32 mNumPrimitive;
+        EmitterSetData* data;
+        EmitterTblData* emitterRef;
+        u32 numEmitter;
+        u32 userData;
+        EmitterSetData* data2;
+        EmitterTblData* emitterRef2;
+        u32 numEmitter2;
+        u32 userData2;
+        const char* name;
+        u8 unused;
+        ParticleShader** shaders;
+        u32 numShader;
+        Primitive** primitives;
+        u32 numPrimitive;
     };
 
     static_assert(sizeof(EmitterSet) == 0x38, "EmitterSet size mismatch");
@@ -48,8 +48,8 @@ public:
     ParticleShader* GetShader(s32 emitterSetID, const VertexShaderKey* vertexShaderKey, const FragmentShaderKey* fragmentShaderKey);
 
     Primitive* GetPrimitive(s32 emitterSetID, u32 index) {
-        u32 numPrimitive = this->mEmitterSets[emitterSetID].mNumPrimitive;
-        Primitive** primitives = this->mEmitterSets[emitterSetID].mPrimitives;
+        u32 numPrimitive = this->emitterSets[emitterSetID].numPrimitive;
+        Primitive** primitives = this->emitterSets[emitterSetID].primitives;
 
         for (u32 i = 0; i < numPrimitive; i++)
             if (primitives[i] != NULL && index == i)
@@ -58,17 +58,17 @@ public:
         return NULL;
     }
 
-    System* mSystem;
-    u32 mResourceID;
-    Header* mResource;
-    char* mStrTbl;
-    u8* mTextureDataTbl;
-    EmitterSet* mEmitterSets;
-    Heap* mHeap;
-    ParticleShader** mShaders;
-    u32 mNumShader;
-    Primitive** mPrimitives;
-    u32 mNumPrimitive;
+    System* system;
+    u32 resourceID;
+    Header* resource;
+    char* strTbl;
+    u8* textureDataTbl;
+    EmitterSet* emitterSets;
+    Heap* heap;
+    ParticleShader** shaders;
+    u32 numShader;
+    Primitive** primitives;
+    u32 numPrimitive;
 };
 
 static_assert(sizeof(Resource) == 0x30, "Resource size mismatch");
