44a244cab821df5262e6c353968263579c5b769e
Luminyx <79218624+Luminyx1@users.noreply.github.com>
2021-09-09 13:04:37 -0400

    mPrefix removal from game/level

--- include/game/level/level.h
+++ include/game/level/level.h
@@ -9,81 +9,81 @@ public:
     class Area {
     public:
         struct Options {
-            u32 mEventBits32;               // _0
-            u32 mEventBits64;               // _4
-            u16 mWrapByte;                  // _8
-            u16 mTimelimit;                 // _A
+            u32 eventBits32;                // _0
+            u32 eventBits64;                // _4
+            u16 wrapValue;                  // _8
+            u16 timelimit;                  // _A
             u8 _C;                          // _C
             u8 _D;                          // _D
             u8 _E;                          // _E
             u8 _F;                          // _F
-            u8 mStartEntrance;              // _10
+            u8 startEntrance;               // _10
             u8 _11;                         // _11
             u8 _12;                         // _12
-            u8 mStartEntranceCoinBoost;     // _13
-            u16 mTimelimit2;                // _14
-            u16 mTimelimit3;                // _16
+            u8 startEntranceCoinBoost;      // _13
+            u16 timelimit2;                 // _14
+            u16 timelimit3;                 // _16
         };
 
         struct Sprite {
-            u16 mType;                      // _0
-            u16 mX;                         // _2
-            u16 mY;                         // _4
-            u16 mEventID1_2;                // _6
-            union { u32 mSettings1;
-                struct {
-                    u32 nybble12 : 4;
-                    u32 nybble11 : 4;
-                    u32 nybble10 : 4;
-                    u32 nybble9  : 4;
-                    u32 nybble8  : 4;
-                    u32 nybble7  : 4;
-                    u32 nybble6  : 4;
-                    u32 nybble5  : 4;
-                };
-            };
-            union { u32 mSettings2;
-                struct {
-                    u32 nybble20 : 4;
-                    u32 nybble19 : 4;
-                    u32 nybble18 : 4;
-                    u32 nybble17 : 4;
-                    u32 nybble16 : 4;
-                    u32 nybble15 : 4;
-                    u32 nybble14 : 4;
-                    u32 nybble13 : 4;
-                };
-            };
-            u8 mZoneID;                     // _10
-            u8 mLayer;                      // _11
-            u8 mMovementID;                 // _12
-            u8 mLinkID;                     // _13
-            u8 mInitialStateFlag;           // _14
+            u16 type;                       // _0
+            u16 x;                          // _2
+            u16 y;                          // _4
+            u16 eventID1_2;                 // _6
+            union { u32 settings1;          // _8
+                struct {                    // _8
+                    u32 nybble12 : 4;       // _8
+                    u32 nybble11 : 4;       // _8
+                    u32 nybble10 : 4;       // _8
+                    u32 nybble9  : 4;       // _8
+                    u32 nybble8  : 4;       // _8
+                    u32 nybble7  : 4;       // _8
+                    u32 nybble6  : 4;       // _8
+                    u32 nybble5  : 4;       // _8
+                };                          // _8
+            };                              // _8
+            union { u32 settings2;          // _C
+                struct {                    // _C
+                    u32 nybble20 : 4;       // _C
+                    u32 nybble19 : 4;       // _C
+                    u32 nybble18 : 4;       // _C
+                    u32 nybble17 : 4;       // _C
+                    u32 nybble16 : 4;       // _C
+                    u32 nybble15 : 4;       // _C
+                    u32 nybble14 : 4;       // _C
+                    u32 nybble13 : 4;       // _C
+                };                          // _C
+            };                              // _C
+            u8 zoneID;                      // _10
+            u8 layer;                       // _11
+            u8 movementID;                  // _12
+            u8 linkID;                      // _13
+            u8 initialStateFlag;            // _14
         };
 
         struct Location {
-            u16 mX;                         // _0
-            u16 mY;                         // _2
-            u16 mW;                         // _4
-            u16 mH;                         // _6
-            u8 mID;                         // _8
+            u16 x;                          // _0
+            u16 y;                          // _2
+            u16 w;                          // _4
+            u16 h;                          // _6
+            u8 id;                          // _8
         };
 
         struct Path {
-            u8 mID;                         // _0
+            u8 id;                          // _0
             s8 _1;                          // _1
-            u16 mStartNodeIdx;              // _2
-            u16 mNodeCount;                 // _4
-            u16 mFlags;                     // _6 (0b10 -> loops)
+            u16 startNodeIdx;               // _2
+            u16 nodeCount;                  // _4
+            u16 flags;                      // _6 (0b10 -> loops)
             u32 _8;                         // _8
         };
 
         struct PathNode {
-            u16 mX;                         // _0
-            u16 mY;                         // _2
-            f32 mSpeed;                     // _4
-            f32 mAccel;                     // _8
-            s16 mDelay;                     // _C
+            u16 x;                          // _0
+            u16 y;                          // _2
+            f32 speed;                      // _4
+            f32 accel;                      // _8
+            s16 delay;                      // _C
             u16 _E;                         // _E
             u8 _10;                         // _10
             u8 _11;                         // _11
@@ -96,10 +96,10 @@ public:
         Path* getPath(u8 id);
         PathNode* getPathNodes(u32 id);
 
-        u32 mID;                            // 0
-        void* mBlocks[15];                  // 4
-        u32 mBlockSizes[15];                // 40
-        u32 mBlockEntryCounts[15];          // 7C
+        u32 id;                             // 0
+        void* blocks[15];                   // 4
+        u32 blockSizes[15];                 // 40
+        u32 blockEntryCounts[15];           // 7C
         u8 _B8[0x100];                      // B8
         u8 _1B8[0x100];                     // 1B8
         u8 _2B8[0x100];                     // 2B8
@@ -108,5 +108,5 @@ public:
 public:
     Area* getArea(u32 id);
 
-    Area mAreas[4];
+    Area areas[4];
 };
--- include/game/level/levelcamera.h
+++ include/game/level/levelcamera.h
@@ -8,15 +8,15 @@ class LevelCamera {
 public:
 	f32 _10;                // 10
 	f32 _14;                // 14
-	Rect mZoneBox;          // 18
+	Rect zoneBox;           // 18
 	Rect _28[4];            // 28
-	f32 mCameraWidth;       // 68
-	f32 mCameraHeight;      // 6C
-	f32 mCameraLeft;        // 70
-	f32 mCameraCenterX;     // 74
-	f32 mCameraRight;       // 78
-	f32 mCameraTop;         // 7C
-	f32 mCameraCenterY;     // 80
-	f32 mCameraBottom;      // 84
+	f32 cameraWidth;        // 68
+	f32 cameraHeight;       // 6C
+	f32 cameraLeft;         // 70
+	f32 cameraCenterX;      // 74
+	f32 cameraRight;        // 78
+	f32 cameraTop;          // 7C
+	f32 cameraCenterY;      // 80
+	f32 cameraBottom;       // 84
     //...
 };
