3a49ec86e41b16ac9d7ffcd17c62f2fe3c6a8529
Luminyx1 <letterable9191@gmail.com>
2021-09-18 15:44:10 -0400

    Some improvements

--- source/respawn.cpp
+++ source/respawn.cpp
@@ -1,9 +1,9 @@
 #include <game/task/taskmgr.h>
-#include <game/task/courseselecttask.h>
+#include <game/task/coursetask.h>
 #include <game/level/levelinfo.h>
 #include <tsuru/tsurusavemgr.h>
 
 void respawn() {
     if (TsuruSaveMgr::sSaveData.instantRespawnEnabled)
-        TaskMgr::instance()->startLevel(CourseSelectTask::instance(), LevelInfo::instance()->world, LevelInfo::instance()->level);
+        TaskMgr::instance()->startLevel(CourseTask::instance(), LevelInfo::instance()->world, LevelInfo::instance()->level);
 }
