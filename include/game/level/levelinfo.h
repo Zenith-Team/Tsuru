635e6880fe16a13e87fd92d2d3881b92364c4860
Luminyx1 <letterable9191@gmail.com>
2021-09-18 23:26:24 -0400

    Add titlescreen themes hack

--- include/game/level/levelinfo.h
+++ include/game/level/levelinfo.h
@@ -2,14 +2,27 @@
 
 #include <sead.h>
 
-class LevelInfo {
+class LevelInfo { // Size: 0x24
     SEAD_SINGLETON_DISPOSER(LevelInfo)
 
 public:
+    LevelInfo();
+
+    void setDestArea(u8 area);
+    void setDestEntrance(s8 entrance);
+    void setDestLevel(u8 level);
+    void setDestWorld(u8 world);
+    void setDestZone(u32 zone);
+
+    void setTitlescreenDestination();
+
+    // Custom function
+    void setTitlescreenThemeEntrance();
+
     u8 world;                   // 10
     u8 level;                   // 11
     u8 area;                    // 12
-    u8 entrance;                // 13
+    s8 entrance;                // 13
     u32 zone;                   // 14
     u32 entranceType;           // 18
     u8 destWorld;               // 1C
@@ -17,5 +30,7 @@ public:
     u8 destArea;                // 1E
     u8 destEntrance;            // 1F
     u32 destZone;               // 20
-    u32 destEntranceType;       // 24
+//  u32 destEntranceType;       // 24
 };
+
+static_assert(sizeof(LevelInfo) == 0x24, "LevelInfo size mismatch");
---
+++ source/titlescreen.cpp
@@ -0,0 +1,20 @@
+#include <game/level/levelinfo.h>
+#include <game/savemgr.h>
+#include <log.h>
+
+void LevelInfo::setTitlescreenThemeEntrance() {
+    switch (SaveMgr::instance()->saveData->saveSlots[SaveMgr::instance()->saveData->header.lastSessionSaveSlot].csPlayerLocation.worldNumber) {
+        case 0: this->setDestEntrance(0); return; // Acorn Plains
+        case 1: this->setDestEntrance(1); return; // Layer-Cake Desert
+        case 2: this->setDestEntrance(2); return; // Sparkling Waters
+        case 3: this->setDestEntrance(3); return; // Frosted Glacier
+        case 4: this->setDestEntrance(4); return; // Soda Jungle
+        case 5: this->setDestEntrance(5); return; // Rock-Candy Mines
+        case 6: this->setDestEntrance(6); return; // Meringue Clouds
+        case 7: this->setDestEntrance(7); return; // Peach's Castle
+        case 8: this->setDestEntrance(8); return; // Superstar Road
+        case 9: this->setDestEntrance(9); return; // Secret Island
+
+        default: LOG("Game saved in unknown world"); this->setDestEntrance(0); return;
+    }
+}
