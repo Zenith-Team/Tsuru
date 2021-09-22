0200aa87e8a992947cb86d24a19cba4d051b8c8a
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-09-21 21:19:31 -0300

    Remove snprintf declaration

--- include/tsuru/utils.h
+++ include/tsuru/utils.h
@@ -4,7 +4,6 @@
 
 extern "C" int strcmp(const char* str1, const char* str2);
 extern "C" int wcscmp(const wchar_t* str1, const wchar_t* str2);
-extern "C" int snprintf(char* buffer, unsigned int length, const char* string, ...);
 
 // Modified version of moveValueTo which supports overflowing/underflowing the value, used for smooth rotations.
 bool moveValueWithOverflowTo(u32 &value, u32 target, u32 step, bool subtract);
