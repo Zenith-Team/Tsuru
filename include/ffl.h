164684f9a1abf4356bb44dd5c108d09d3f000759
Luminyx1 <letterable9191@gmail.com>
2021-09-29 16:17:43 -0400

    Update ffl.h

--- include/ffl.h
+++ include/ffl.h
@@ -10,7 +10,8 @@ struct AuthorID { // Size: 0x8
     u16 _4;
     u16 _6;
 };
-static_assert(sizeof(FFL::AuthorID) == 0x8, "FFL::AuthorID size mismatch"); //* Correct
+
+static_assert(sizeof(FFL::AuthorID) == 0x8, "FFL::AuthorID size mismatch");
 
 struct CreateIDBase { // Size: 0x6
     u8 _0;
@@ -19,7 +20,8 @@ struct CreateIDBase { // Size: 0x6
     u8 _3;
     u16 authorIDCRC16;
 };
-static_assert(sizeof(FFL::CreateIDBase) == 0x6, "FFL::CreateIDBase size mismatch"); //* Correct
+
+static_assert(sizeof(FFL::CreateIDBase) == 0x6, "FFL::CreateIDBase size mismatch");
 
 struct CreateID { // Size: 0xA
     u8 flags;
@@ -28,7 +30,8 @@ struct CreateID { // Size: 0xA
     u8 index;
     CreateIDBase createIDBase;
 };
-static_assert(sizeof(FFL::CreateID) == 0xA, "FFL::CreateID size mismatch"); //* Correct
+
+static_assert(sizeof(FFL::CreateID) == 0xA, "FFL::CreateID size mismatch");
 
 struct MiiDataCore { // Size: 0x48
     u32 _0;              // 0
@@ -46,13 +49,15 @@ struct MiiDataCore { // Size: 0x48
     u32 _40;
     u32 _44;
 };
-static_assert(sizeof(FFL::MiiDataCore) == 0x48, "FFL::MiiDataCore size mismatch"); //* Correct
+
+static_assert(sizeof(FFL::MiiDataCore) == 0x48, "FFL::MiiDataCore size mismatch");
 
 struct MiiData { // Size: 0x5C
     MiiDataCore miiDataCore;
     wchar_t creatorName[10];
 };
-static_assert(sizeof(FFL::MiiData) == 0x5C, "FFL::MiiData size mismatch"); //* Correct
+
+static_assert(sizeof(FFL::MiiData) == 0x5C, "FFL::MiiData size mismatch");
 
 // A complete Mii character object
 struct StoreData { // Size: 0x60
@@ -60,6 +65,7 @@ struct StoreData { // Size: 0x60
     u16 _5C;         // 5C
     u16 CRC16;       // 5E
 };
-static_assert(sizeof(FFL::StoreData) == 0x60, "FFL::StoreData size mismatch"); //* Correct
+
+static_assert(sizeof(FFL::StoreData) == 0x60, "FFL::StoreData size mismatch");
 
 }
