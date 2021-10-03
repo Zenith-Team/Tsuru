0441e45de3985fc5792935f4df277eab681727fd
Luminyx1 <letterable9191@gmail.com>
2021-10-03 18:44:04 -0400

    sorry jh master

--- source/actors/angrysun.cpp
+++ source/actors/angrysun.cpp
@@ -236,7 +236,7 @@ void AngrySun::beginState_Swoop() {
 
     f32 denom       = (x1 - x2) * (x1 - x3) * (x2 - x3);
     this->swoopA    = (x3 * (y2 - y1) + x2 * (y1 - y3) + x1 * (y3 - y2)) / denom;
-	this->swoopB    = (x3*x3 * (y1 - y2) + x2*x2 * (y3 - y1) + x1*x1 * (y2 - y3)) / denom;
+	this->swoopB    = (x3 * x3 * (y1 - y2) + x2 * x2 * (y3 - y1) + x1 * x1 * (y2 - y3)) / denom;
     this->swoopC    = (x2 * x3 * (x2 - x3) * y1 + x3 * x1 * (x3 - x1) * y2 + x1 * x2 * (x1 - x2) * y3) / denom;
 
     this->speed.x = x3 * 0.025f;
