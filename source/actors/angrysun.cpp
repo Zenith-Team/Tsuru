c6dbab90fe6bbb43514d68e992405bd8aa47fbaa
Luminyx <79218624+Luminyx1@users.noreply.github.com>
2021-10-04 17:01:03 -0400

    Update README.md

--- source/actors/angrysun.cpp
+++ source/actors/angrysun.cpp
@@ -246,10 +246,10 @@ void AngrySun::beginState_Swoop() {
 	y2 = 0.0f;
     y3 = y1;
 
-    f32 denom       = (x1 - x2) * (x1 - x3) * (x2 - x3);
-    this->swoopA    = (x3 * (y2 - y1) + x2 * (y1 - y3) + x1 * (y3 - y2)) / denom;
-	this->swoopB    = (x3 * x3 * (y1 - y2) + x2 * x2 * (y3 - y1) + x1 * x1 * (y2 - y3)) / denom;
-    this->swoopC    = (x2 * x3 * (x2 - x3) * y1 + x3 * x1 * (x3 - x1) * y2 + x1 * x2 * (x1 - x2) * y3) / denom;
+    f32 denom    = (x1 - x2) * (x1 - x3) * (x2 - x3);
+    this->swoopA = (x3 * (y2 - y1) + x2 * (y1 - y3) + x1 * (y3 - y2)) / denom;
+	this->swoopB = (x3 * x3 * (y1 - y2) + x2 * x2 * (y3 - y1) + x1 * x1 * (y2 - y3)) / denom;
+    this->swoopC = (x2 * x3 * (x2 - x3) * y1 + x3 * x1 * (x3 - x1) * y2 + x1 * x2 * (x1 - x2) * y3) / denom;
 
     this->speed.x = x3 * 0.025f;
 }
