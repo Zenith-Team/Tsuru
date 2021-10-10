38fb8023110669132e879462a260b9418a9e27f7
Luminyx <79218624+Luminyx1@users.noreply.github.com>
2021-10-10 12:41:56 -0400

    Add some more error logging to customsavemgr

--- source/save/customsavemgr.cpp
+++ source/save/customsavemgr.cpp
@@ -32,15 +32,19 @@ bool CustomSaveMgr::write() {
     sead::FileHandle handle;
     sead::FileDevice* device = sead::FileDeviceMgr::instance()->tryOpen(&handle, this->filepath, sead::FileDevice::FileOpenFlag_Create, 0);
     
-    if (!handle.device)
+    if (!handle.device) {
+        LOG("Handle device was null.");
         return false;
+    }
     
     device->tryWrite(&bytesWritten, &handle, reinterpret_cast<u8*>(this->savestruct), this->getSaveDataSize());
 
-    if (bytesWritten != this->getSaveDataSize())
+    if (bytesWritten != this->getSaveDataSize()) {
+        LOG("Write size mismatch, wrote size: 0x%x, expected size: 0x%x", bytesWritten, this->getSaveDataSize());
         return false;
-    else
-        return true;
+    }
+
+    else return true;
 }
 
 void CustomSaveMgr::init() {
@@ -55,8 +59,10 @@ void CustomSaveMgr::init() {
     if (!readHandle.device) {
         LOG("Custom save file does not exist. Creating...");
 
-        if (!this->write())
+        if (!this->write()) {
+            LOG("Write failed.");
             return;
+        }
     }
 
     else if (bytesRead != this->getSaveDataSize()) {
@@ -75,8 +81,10 @@ void CustomSaveMgr::init() {
 }
 
 void CustomSaveMgr::save() {
-    if (!this->inited)
+    if (!this->inited){
+        LOG("Save manager at %x was not inited before saving. Returning...", this);
         return;
-    else
-        this->write();
+    }
+    
+    this->write();
 }
