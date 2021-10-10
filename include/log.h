374f9930399b244b6ce024f004a7dfafce6839da
Luminyx1 <letterable9191@gmail.com>
2021-10-09 21:03:45 -0400

    Update log.h

--- include/log.h
+++ include/log.h
@@ -2,7 +2,7 @@
 
 #ifdef Cemu
 #include <dynlibs/os/functions.h>
-#define LOG(FMT, ARGS ...)  __os_snprintf(logMsg, sizeof(logMsg), FMT, ## ARGS); OSConsoleWrite(logMsg, sizeof(logMsg))
+#define LOG(FMT, ...)  __os_snprintf(logMsg, sizeof(logMsg), FMT, ## __VA_ARGS__); OSConsoleWrite(logMsg, sizeof(logMsg))
 #else
-#define LOG(FMT, ARGS ...)  ((void (*)(const char* format, ...))(*(unsigned int*)(DATA_ADDR - 4)))(FMT, ## ARGS)
+#define LOG(FMT, ...) ((void (*)(const char* format, ...))(*(unsigned int*)(DATA_ADDR - 4)))(FMT, ## __VA_ARGS__)
 #endif
