b8574cb4569182801f0b8fd06a9ecc4174229f08
Luminyx1 <letterable9191@gmail.com>
2021-10-10 14:32:49 -0400

    Try adding sizemodifier bool to coursetask but miserably failed
    This is an upload worthy glitch lol

--- include/game/task/coursetask.h
+++ include/game/task/coursetask.h
@@ -2,8 +2,11 @@
 
 #include <sead/task.h>
 
-class CourseTask : public sead::CalculateTask {
+class CourseTask : public sead::CalculateTask { // Size: 0x5A8 (custom) 0x5A4 (original)
     SEAD_SINGLETON_TASK(CourseTask)
 
 public:
+    u8 _4[0x5A4 - 0x4];
+    bool sizeModifierEnabled;   // Custom member
 };
+
