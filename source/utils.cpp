c3a75bcbf7cdab1b044f0f66aa77fd3672c9eb6c
Luminyx1 <letterable9191@gmail.com>
2021-10-24 12:40:19 -0400

    Update utils

--- source/utils.cpp
+++ source/utils.cpp
@@ -4,9 +4,10 @@
 
 // Compare if two wide strings are equal
 extern "C" s32 wcscmp(const wchar_t* str1, const wchar_t* str2) {
-    while (*str1 == *str2++)
+    while (*str1 == *str2++) {
         if (*str1++ == 0)
             return 0;
+    }
 
     return (*str1 - *--str2);
 }
@@ -31,10 +32,12 @@ bool moveValueWithOverflowTo(u32& value, u32 target, u32 step, bool subtract) {
 
 // Inline function static implementation for branching to from asm
 
-bool nthBitRight(u32 val, u32 nth) {
-    return (val >> (nth - 1)) & 1;
-}
+bool nthBit64Right(u64 val, u64 nth) { return (val >> (nth - 1)) & 1; }
+bool nthBit32Right(u32 val, u32 nth) { return (val >> (nth - 1)) & 1; }
+bool nthBit16Right(u16 val, u16 nth) { return (val >> (nth - 1)) & 1; }
+bool nthBit8Right(u8 val, u8 nth)    { return (val >> (nth - 1)) & 1; }
 
-bool nthBitLeft(u32 val, u32 nth) {
-    return (val << (nth - 1)) & 1;
-}
+bool nthBit64Left(u64 val, u64 nth) { return (val << (nth - 1)) & 1; }
+bool nthBit32Left(u32 val, u32 nth) { return (val << (nth - 1)) & 1; }
+bool nthBit16Left(u16 val, u16 nth) { return (val << (nth - 1)) & 1; }
+bool nthBit8Left(u8 val, u8 nth)    { return (val << (nth - 1)) & 1; }
