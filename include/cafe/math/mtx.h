343af2b0cbaf89fa9d19805fcdcd3d4d56c97a6f
Luminyx <79218624+Luminyx1@users.noreply.github.com>
2021-10-31 10:09:20 -0400

    Death foo fixed + cafe math

---
+++ include/cafe/math/mtx.h
@@ -0,0 +1,24 @@
+#pragma once
+
+typedef unsigned int u32;
+class Mtx34;
+class Mtx44;
+template <typename T>
+class Vec3;
+typedef Vec3<float> Vec3f;
+
+extern "C" {
+
+// MTX34
+void ASM_MTXConcat(Mtx34* a, Mtx34* b, Mtx34* out);
+u32  ASM_MTXInverse(Mtx34* src, Mtx34* dst);
+void ASM_MTXMultVec(Mtx34* mtx, Vec3f* vec, Vec3f* out);
+
+// MTX44
+void ASM_MTX44Copy(Mtx44* src, Mtx44* dst);
+void ASM_MTX44Identity(Mtx44* mtx);
+void ASM_MTX44Concat(Mtx44* a, Mtx44* b, Mtx44* out);
+void ASM_MTX44Transpose(Mtx44* mtx, Mtx44* out);
+void ASM_MTX44MultVec(Mtx44* mtx, Vec3f* vec, Vec3f* out);
+
+}
--- source/profileres.cpp
+++ source/profileres.cpp
@@ -1,4 +1,8 @@
 #include <game/profile/profile.h>
 
+// Add archive links for unused actors without resources
 PROFILE_RESOURCES(Profile::spriteToProfileList[312], "cube_kinoko");
 PROFILE_RESOURCES(Profile::spriteToProfileList[463], "bowser_switch");
+
+// Additional resources for existing actors
+PROFILE_RESOURCES(Profile::spriteToProfileList[229], "huhu", "huhd");
