99b59f915dfca7afdf1060c6e8476d6cf2bc2de5
Luminyx1 <letterable9191@gmail.com>
2021-11-14 14:04:03 -0500

    Ayo?? FrameBuffer??

--- include/agl/renderbuffer.h
+++ include/agl/renderbuffer.h
@@ -2,27 +2,22 @@
 
 #include <sead/idisposer.h>
 #include <sead/framebuffer.h>
-#include <sead/color.h>
 #include <agl/rendertargetcolor.h>
 #include <agl/rendertargetdepth.h>
 
-//! This header is highly inaccurate due to the equivalent inaccuracy of sead/framebuffer.h
-//! Fix that first and then fix this
-
 namespace agl {
 
-class RenderBuffer : public sead::IDisposer { // Size: 0x50, //? Does this inherit sead::FrameBuffer?
+class RenderBuffer : public sead::IDisposer, public sead::FrameBuffer { // Size: 0x50
 public:
     RenderBuffer();
     RenderBuffer(Vec2f& size, f32, f32, f32 sizeX, f32 sizeY);
     virtual ~RenderBuffer();
 
-    SEAD_RTTI_BASE(RenderBuffer)
-    virtual void copyToDisplayerBuffer(void* displayBuffer);
-    virtual void clear(u32, u32 flags, sead::Color4f& color, f32 depthValue, u32 stencilValue) const;
+    SEAD_RTTI_OVERRIDE(RenderBuffer, sead::FrameBuffer)
+    virtual void copyToDisplayBuffer(const sead::DisplayBuffer* displayBuffer);
+    virtual void clear(u32, u32, const sead::Color4f& color, f32 depthValue, u32 stencilValue);
     virtual void bindImpl_();
 
-    sead::FrameBuffer frameBuffer; //* Remove this if it *does* inherit sead::FrameBuffer
     agl::RenderTargetColor* targetColors[8];
     agl::RenderTargetDepth* targetDepth;
 };
--- include/sead/framebuffer.h
+++ include/sead/framebuffer.h
@@ -1,23 +1,20 @@
 #pragma once
 
-//! TODO: I hate this entire header it's so confusing and I cannot understand anything everything is inlined and the inheritance hierarchy is weird please someone smarter than me fix it :)
-//? EDIT: Deleted it all in favor of lazy padding, still requires completion
-
 #include <sead/boundbox.h>
 #include <sead/runtimetypeinfo.h>
-#include <dynlibs/gx2/types.h>
+#include <sead/color.h>
 
 namespace sead {
 
-class FrameBuffer { // Inherits sead::LogicalFrameBuffer
-    SEAD_RTTI_BASE(FrameBuffer)
+class DisplayBuffer;
+
+class LogicalFrameBuffer { // Size: 0x1C
+    SEAD_RTTI_BASE(LogicalFrameBuffer)
 
 public:
-    ~FrameBuffer();
-
     virtual void vf1C(); // deleted
-	virtual void copyToDisplayBuffer(void* displayBuffer);
-	virtual void clear(u32 flags, Color4f* color, u32 stencilValue, f32 depthValue);
+	virtual void copyToDisplayBuffer(const DisplayBuffer* displayBuffer);
+	virtual void clear(u32, u32, const Color4f& color, f32 depthValue, u32 stencilValue);
 	virtual void vf34(); // deleted
 	virtual void bindImpl_();
 
@@ -27,7 +24,9 @@ public:
     BoundBox2<f32> physicalArea;
 };
 
-// TODO: Verify this size
+class FrameBuffer : public LogicalFrameBuffer { // Size: 0x1C
+};
+
 static_assert(sizeof(FrameBuffer) == 0x1C, "sead::FrameBuffer size mismatch");
 
 }
