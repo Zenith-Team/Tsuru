a066a3360ea483965fcce72e098ee21bfe66c8d9
Luminyx1 <letterable9191@gmail.com>
2021-11-27 22:31:18 -0500

    Make special physics code clearer

--- source/specialphysics.cpp
+++ source/specialphysics.cpp
@@ -7,25 +7,20 @@ extern f32 MiniPlayerJumpArc;
 extern f32 PlayerJumpMax;
 extern f32 PlayerDescentRate;
 
+enum WrapFlags {
+    Wrap        = 1 << 0,
+    LowGravity  = 1 << 1
+};
+
 void AreaTask::setPhysicsValues() {
     Level::Area* area = Level::instance()->getArea(LevelInfo::instance()->area);
     Level::Area::Options* areaOptions = reinterpret_cast<Level::Area::Options*>(area->blocks[1]);
 
-    // Wrap flag set to "wrap"
-    if (areaOptions->wrapValue & 1) this->wrapFlag = 1;
-
-    // Wrap flag set to "low-gravity"
-    else if (areaOptions->wrapValue & 2) {
-        PlayerJumpArc = 0.5f;
-        MiniPlayerJumpArc = 0.5f;
-        PlayerJumpMax = 4.5f;
-        PlayerDescentRate = -2.0f;
+    if (areaOptions->wrapValue & Wrap) {
+        this->wrapFlag = 1;
     }
 
-    // Wrap flag set to "wrap + low-gravity"
-    else if (areaOptions->wrapValue & 3) {
-        this->wrapFlag = 1;
-
+    else if (areaOptions->wrapValue & LowGravity) {
         PlayerJumpArc = 0.5f;
         MiniPlayerJumpArc = 0.5f;
         PlayerJumpMax = 4.5f;
