78930e02d16210e7ba8adb6a651de1128cef64d0
Luminyx1 <letterable9191@gmail.com>
2021-11-30 20:11:39 -0500

    Add glow block profile res

--- source/profileres.cpp
+++ source/profileres.cpp
@@ -3,6 +3,7 @@
 // Add archive links for unused actors without resources
 PROFILE_RESOURCES(Profile::spriteToProfileList[312], "cube_kinoko");
 PROFILE_RESOURCES(Profile::spriteToProfileList[463], "bowser_switch");
+PROFILE_RESOURCES(Profile::spriteToProfileList[199], "block_light", "light_block");
 
 // Additional resources for existing actors
 PROFILE_RESOURCES(Profile::spriteToProfileList[229], "huhu", "huhd");
