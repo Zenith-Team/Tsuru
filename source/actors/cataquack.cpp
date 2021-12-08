6180770cd677c221724290d5c85e58aefd311702
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2021-12-08 15:10:13 -0300

    unfinished cataquack that im gonna need to commit to fix merge conflict 💀

--- source/actors/cataquack.cpp
+++ source/actors/cataquack.cpp
@@ -5,6 +5,7 @@
 #include <game/util.h>
 #include <log.h>
 #include <tsuru/utils.h>
+#include <math/functions.h>
 
 class Cataquack : public Enemy {
 public:
@@ -18,14 +19,19 @@ public:
     u32 onDraw() override;
 
     void updateModel();
+    void beginChase();
+    void endChase();
 
     ModelWrapper* model;
+    bool chasing;
 
-    DECLARE_STATE(Cataquack, IdleWalking);
+    DECLARE_STATE(Cataquack, Walk);
     DECLARE_STATE(Cataquack, Turn);
+    //DECLARE_STATE(Cataquack, Launch);
 };
-CREATE_STATE(Cataquack, IdleWalking);
+CREATE_STATE(Cataquack, Walk);
 CREATE_STATE(Cataquack, Turn);
+//CREATE_STATE(Cataquack, Launch);
 
 const ActorInfo CataquackActorInfo = {
     Vec2i(8, -8), Vec2i(8, -8), Vec2i(32, 32), 0, 0, 0, 0, 0
@@ -33,7 +39,11 @@ const ActorInfo CataquackActorInfo = {
 const Profile CataquackProfile(&Cataquack::build, ProfileID::Cataquack, "Cataquack", &CataquackActorInfo, Profile::Flag_DontRenderOffScreen);
 PROFILE_RESOURCES(ProfileID::Cataquack, "poihana");
 
-Cataquack::Cataquack(const ActorBuildInfo* buildInfo) : Enemy(buildInfo) { };
+Cataquack::Cataquack(const ActorBuildInfo* buildInfo)
+    : Enemy(buildInfo)
+    , model(nullptr)
+    , chasing(false)
+{ };
 
 Actor* Cataquack::build(const ActorBuildInfo* buildInfo) {
     return new Cataquack(buildInfo);
@@ -48,12 +58,13 @@ u32 Cataquack::onCreate() {
 
     PhysicsMgr::Sensor belowSensor = { -6,  6,  0 };
     PhysicsMgr::Sensor aboveSensor = { -6,  6, 28 };
-    PhysicsMgr::Sensor sidesSensor = {  4, 20, 12 };
+    PhysicsMgr::Sensor sidesSensor = {  8, 24, 12 };
     this->physicsMgr.init(this, &belowSensor, &aboveSensor, &sidesSensor);
 
     this->direction = this->directionToPlayerH(this->position);
+    this->rotation.y = Direction::directionToRotationList[this->direction];
 
-    doStateChange(&Cataquack::StateID_IdleWalking);
+    this->doStateChange(&Cataquack::StateID_Walk);
     this->updateModel();
     return 1;
 }
@@ -61,6 +72,7 @@ u32 Cataquack::onCreate() {
 u32 Cataquack::onExecute() {
     this->states.execute();
     this->updateModel();
+    this->deleteActorWhenOutOfView(0); //! UNTESTED
     return 1;
 }
 
@@ -78,23 +90,39 @@ void Cataquack::updateModel() {
     this->model->updateModel();
 }
 
-/* STATE: IdleWalking */
+/* STATE: Walk */
 
-void Cataquack::beginState_IdleWalking() {
-    this->speed.x    = this->direction == Direction::Left ? -0.5f : 0.5f;
-    this->maxSpeed.x = this->direction == Direction::Left ? -0.5f : 0.5f;
+void Cataquack::beginState_Walk() {
+    f32 speed = this->chasing ? 1.0f : 0.5f;
+    this->speed.x    = this->direction == Direction::Left ? -speed : speed;
+    this->maxSpeed.x = this->direction == Direction::Left ? -speed : speed;
 }
 
-void Cataquack::executeState_IdleWalking() {
+void Cataquack::executeState_Walk() {
     this->handleGravity();
     this->handleSpeed();
     this->physicsMgr.processCollisions();
 
+    Vec2f distToPlayer;
+    if (this->distanceToPlayer(distToPlayer) > -1 && fabs(distToPlayer.x) < 8.0f * 16.0f && fabs(distToPlayer.y) < 6.0f * 16.0f) {
+        if ((this->direction == Direction::Left && distToPlayer.x < 0) || (this->direction == Direction::Right && distToPlayer.x > 0)) {
+            this->beginChase();
+        }
+
+        if (this->chasing) {
+            if ((this->direction == Direction::Left && distToPlayer.x > 0) || (this->direction == Direction::Right && distToPlayer.x < 0)) {
+                this->doStateChange(&Cataquack::StateID_Turn);
+            }
+        }
+    } else {
+        this->endChase();
+    }
+
     if (this->physicsMgr.isOnGround()) this->speed.y = 0.0f;
-    if (this->physicsMgr.isCollided(this->direction)) doStateChange(&Cataquack::StateID_Turn);
+    if (this->physicsMgr.isCollided(this->direction) && !this->chasing) this->doStateChange(&Cataquack::StateID_Turn);
 }
 
-void Cataquack::endState_IdleWalking() {}
+void Cataquack::endState_Walk() { }
 
 /* STATE: Turn */
 
@@ -105,11 +133,36 @@ void Cataquack::beginState_Turn() {
 }
 
 void Cataquack::executeState_Turn() {
-    if (moveValueWithOverflowTo(this->rotation.y, Direction::directionToRotationList[this->direction], 0x2000000, this->direction)) {
-        doStateChange(&this->StateID_IdleWalking);
+    this->handleGravity();
+    this->handleSpeed();
+    this->physicsMgr.processCollisions();
+    if (this->physicsMgr.isOnGround()) this->speed.y = 0.0f;
+
+    u32 step = this->chasing ? fixDeg(6.0f) : fixDeg(3.0f);
+    if (moveValueWithOverflowTo(this->rotation.y, Direction::directionToRotationList[this->direction], step, this->direction)) {
+        this->doStateChange(&Cataquack::StateID_Walk);
     }
 }
 
 void Cataquack::endState_Turn() {
     this->rotation.y = Direction::directionToRotationList[this->direction];
+}
+
+void Cataquack::beginChase() {
+    this->chasing    = true;
+    this->speed.x    = this->direction == Direction::Left ? -1.0f : 1.0f;
+    this->maxSpeed.x = this->direction == Direction::Left ? -1.0f : 1.0f;
+    this->model->sklAnims[0]->speed = 2.0f;
+}
+
+    //Vec2f distToPlayer;
+    //if (this->distanceToPlayer(distToPlayer) < 0 || (fabs(distToPlayer.x) > 8.0f * 16.0f || fabs(distToPlayer.y) > 6.0f * 16.0f)) {
+    //    if ((this->direction == Direction::Left && distToPlayer.x < 0) || (this->direction == Direction::Right && distToPlayer.x > 0)) {
+    //        this->doStateChange(&Cataquack::StateID_Walk);
+
+void Cataquack::endChase() {
+    this->chasing    = false;
+    this->speed.x    = this->direction == Direction::Left ? -0.5f : 0.5f;
+    this->maxSpeed.x = this->direction == Direction::Left ? -0.5f : 0.5f;
+    this->model->sklAnims[0]->speed = 1.0f;
 }
