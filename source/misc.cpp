fc1f6c281345c870419fb95e3cafdaf1e57628e1
Luminyx <79218624+Luminyx1@users.noreply.github.com>
2021-12-10 17:25:52 -0500

    Add current graphics context

--- source/misc.cpp
+++ source/misc.cpp
@@ -2,8 +2,13 @@
 #include "game/task/coursetask.h"
 #include "game/level/levelinfo.h"
 #include "tsuru/save/managers/tsurusavemgr.h"
+#include "sead/graphicscontext.h"
 
 void respawn() {
     if (TsuruSaveMgr::sSaveData.instantRespawnEnabled)
         TaskMgr::instance()->startLevel(CourseTask::instance(), LevelInfo::instance()->world, LevelInfo::instance()->level);
 }
+
+// Always a copy of the currently applied graphics context
+// NEVER write to this, only read
+volatile sead::GraphicsContext CurrentGraphicsContext;
