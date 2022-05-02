d1d100d509c502abaa019ec3b6af83ec541b94fd
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-05-02 05:12:00 -0300

    better loadResource

--- include/game/resource/util.h
+++ include/game/resource/util.h
@@ -4,16 +4,22 @@
 #include "game/resource/resmgr.h"
 #include "log.h"
 
-static inline bool loadResource(const sead::SafeString& name, const sead::SafeString& path, bool isYaz0 = true) {
+static inline bool loadResource(const sead::SafeString& name, const sead::SafeString& path, bool isYaz0 = true, const char* requester = "*") {
+    LOG("%s(TsuruResLoader:%s) Attemping to load resource %s", LogColor::Yellow, requester, path.cstr());
     bool validResource = ResMgr::instance()->loadRes(name, path, nullptr, isYaz0);
     if (!validResource) {
-        LOG("%sFailed to load resource: %s (from file: %s)", LogColor::Red, name.cstr(), path.cstr());
+        LOG("%s(TsuruResLoader:%s) Failed to load resource %s", LogColor::Red, requester, path.cstr());
         return false;
     }
-    bool validArchiveRes = ResArchiveMgr::instance()->loadResArchive(name, path, nullptr);
+    if (ResArchiveMgr::instance()->get(name)) {
+        LOG("%s(TsuruResLoader:%s) Resource %s is already loaded.", LogColor::LightGreen, requester, path.cstr());
+        return true;
+    }
+    bool validArchiveRes = ResArchiveMgr::instance()->loadResArchive(name, name, nullptr);
     if (!validArchiveRes) {
-        LOG("%sFailed to load archive resource: %s (from file: %s)", LogColor::Red, name.cstr(), path.cstr());
+        LOG("%s(TsuruResLoader:%s) Failed to load archive resource %s", LogColor::Red, requester, name.cstr());
         return false;
     }
+    LOG("%s(TsuruResLoader:%s) Successfully loaded resource %s", LogColor::Green, requester, path.cstr());
     return true;
 }
