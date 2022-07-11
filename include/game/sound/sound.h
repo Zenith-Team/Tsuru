4b1b06f2e4019c102f32778680644ed49eb7532e
Luminyx1 <letterable9191@gmail.com>
2022-07-11 11:37:51 -0400

    Update sound.h

--- include/game/sound/sound.h
+++ include/game/sound/sound.h
@@ -28,9 +28,7 @@ inline void playSound(const char* label, const Vec2f& position) {
 // @param label Name of the sound to be played
 // @param position Position to play the sound at
 inline void playSound(const char* label, const Vec3f& position) {
-    Vec2f screenPos;
-    mapPositionToScreen(screenPos, Vec2f(position.x, position.y));
-    BasicSoundActor::GlobalBasicSoundActorB->playSound(label, &screenPos, 0);
+    playSound(label, Vec2f(position.x, position.y));
 }
 
 // Plays a sound at a position
