f7b6130dc4650e26dda18ac5b61f5b3d9dc37ae4
Luminyx1 <79218624+Luminyx1@users.noreply.github.com>
2023-07-29 00:59:10 -0400

    Document a GlobalStateKeeper field

--- include/game/globalstatekeeper.h
+++ include/game/globalstatekeeper.h
@@ -2,13 +2,21 @@
 
 #include "sead/idisposer.h"
 
+ENUM_CLASS(LevelExitMethod,
+    Quit = 1,
+    Win = 2,
+    Die = 3
+);
+
 class GlobalStateKeeper { // Size: 0x21E18 
     SEAD_SINGLETON_DISPOSER(GlobalStateKeeper);
 
 public:
     GlobalStateKeeper();
 
-    u8 _10[0x1A8-0x10];
+    u8 _10[0x20-0x10];
+    LevelExitMethod::__type__ levelExitMethod;
+    u8 _24[0x1A8-0x24];
     u32 _1A8;
     u8 _1AC[0x1B8-0x1AC];
     u32 _1B8;
