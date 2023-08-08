db665b053fb24d9ec9cce4f88571189fbbd0fd4b
Luminyx1 <79218624+Luminyx1@users.noreply.github.com>
2023-08-08 18:19:32 -0400

    Update profileres.cpp

--- source/profileres.cpp
+++ source/profileres.cpp
@@ -4,3 +4,6 @@
 PROFILE_RESOURCES(Profile::sprite(312), Profile::LoadResourcesAt::Course, "cube_kinoko");
 PROFILE_RESOURCES(Profile::sprite(463), Profile::LoadResourcesAt::Course, "bowser_switch");
 PROFILE_RESOURCES(Profile::sprite(199), Profile::LoadResourcesAt::Course, "block_light", "light_block");
+
+// Additional resources
+PROFILE_RESOURCES(ProfileID::Player, Profile::LoadResourcesAt::Boot, "MarioMdl", "LuigiMdl", "KinopioMdl", "MiiMdl", "CommonAnm", "LuigiAnm", "KinopioAnm", "MiiAnm", "EF_HipdropEX", /* Custom: */ "hmrpy");
