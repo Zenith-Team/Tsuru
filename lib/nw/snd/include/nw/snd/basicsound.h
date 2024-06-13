1e13a9b209b5e2bfb09583d0304bb53f0b0f6fde
STUPID Modder <stupidmoddersoup@gmail.com>
2024-06-13 02:42:14 -0300

    add some funcs to nw::snd::internal::BasicSound and nw::snd::SoundHandle

--- lib/nw/snd/include/nw/snd/basicsound.h
+++ lib/nw/snd/include/nw/snd/basicsound.h
@@ -22,6 +22,37 @@ public:
     struct AmbientInfo { };
 
     void Stop(s32 fadeFrames);
+
+    void SetVolume(f32 volume, s32 frames = 0);
+    void SetPitch(f32 pitch);
+
+    void SetLpfFreq(f32 lpfFreq)
+    {
+        m_LpfFreq = lpfFreq;
+    }
+
+    void SetBiquadFilter(s32 type, f32 value)
+    {
+        m_BiquadFilterType = static_cast<s8>(type);
+        m_BiquadFilterValue = value;
+    }
+
+    void SetOutputLine(u32 lineFlag);
+    void ResetOutputLine();
+    void SetPan(f32 pan);
+
+    u8 _0[0xCC];
+    u8 m_Priority;
+    s8 m_BiquadFilterType;
+    u32 m_Id;
+    f32 m_InitVolume;
+    f32 m_Pitch;
+    f32 m_LpfFreq;
+    f32 m_BiquadFilterValue;
+    u32 m_OutputLineFlag;
+    // ...
 };
+
+//static_assert(sizeof(BasicSound) == 0x1A8, "nw::snd::internal::BasicSound size mismatch");
 
 } } }
--- lib/nw/snd/include/nw/snd/soundhandle.h
+++ lib/nw/snd/include/nw/snd/soundhandle.h
@@ -19,6 +19,44 @@ public:
             sound->Stop(fadeFrames);
     }
 
+    void SetVolume(f32 volume, s32 frames = 0)
+    {
+        if (IsAttachedSound())
+            sound->SetVolume(volume, frames);
+    }
+
+    void SetPitch(f32 pitch)
+    {
+        if (IsAttachedSound())
+            sound->SetPitch(pitch);
+    }
+
+    void SetLpfFreq(f32 lpfFreq)
+    {
+        if (IsAttachedSound())
+            sound->SetLpfFreq(lpfFreq);
+    }
+
+    void SetBiquadFilter(s32 type, f32 value)
+    {
+        //NW_MINMAX_ASSERT(type, BIQUAD_FILTER_TYPE_MIN , BIQUAD_FILTER_TYPE_MAX);
+
+        if (IsAttachedSound())
+            sound->SetBiquadFilter(type, value);
+    }
+
+    void SetOutputLine(u32 lineFlag)
+    {
+        if (IsAttachedSound())
+            sound->SetOutputLine(lineFlag);
+    }
+
+    void ResetOutputLine()
+    {
+        if (IsAttachedSound())
+            sound->ResetOutputLine();
+    }
+
     bool IsAttachedSound() const {
         return sound != nullptr;
     }
